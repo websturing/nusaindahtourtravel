6606ffed8bbef907aad086d273c51a371515cde4
websturing <websturing.project@gmail.com>
2020-04-15 01:32:12 +0700

    update Url npm to my.id

--- resources/js/url.js
+++ resources/js/url.js
@@ -1,6 +1,6 @@
-const urlAxios = "http://localhost/majestic_travel/api";
-const urlBase = "majestic_travel";
-const urlWeb = "http://localhost/majestic_travel";
+const urlAxios = "http://tukangketik.my.id/nusaindahtourtravel/api";
+const urlBase = "nusaindahtourtravel";
+const urlWeb = "http://tukangketik.my.id/nusaindahtourtravel";
 
 export default {
     urlAxios: urlAxios,
