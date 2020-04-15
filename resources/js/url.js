3b2190cfdc622a91a653dea9e10eb15e7be7e8d7
websturing <websturing.project@gmail.com>
2020-04-15 10:15:32 +0700

    change url to online

--- resources/js/url.js
+++ resources/js/url.js
@@ -1,10 +1,10 @@
-// const urlAxios = "http://tukangketik.my.id/nusaindahtourtravel/api";
-// const urlBase = "nusaindahtourtravel";
-// const urlWeb = "http://tukangketik.my.id/nusaindahtourtravel";
+const urlAxios = "http://tukangketik.my.id/nusaindahtourtravel/api";
+const urlBase = "nusaindahtourtravel";
+const urlWeb = "http://tukangketik.my.id/nusaindahtourtravel";
 
-const urlAxios = "http://localhost/majestic_travel/api";
-const urlBase = "majestic_travel";
-const urlWeb = "http://localhost/majestic_travel";
+// const urlAxios = "http://localhost/majestic_travel/api";
+// const urlBase = "majestic_travel";
+// const urlWeb = "http://localhost/majestic_travel";
 
 export default {
     urlAxios: urlAxios,
